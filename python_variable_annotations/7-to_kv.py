a3ad95dfae0a6680afe4096b964a8fde4c6d5199
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 14:23:02 +1000

    Implement task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Module converting to key-value tuple."""
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Method  to return  a tuple of string and square of a float"""
+    return (k, v ** 2)
