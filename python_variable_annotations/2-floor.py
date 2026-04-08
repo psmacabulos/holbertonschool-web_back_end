6390de48b12ec6199ff1d9123fe56ed5cd92dcc1
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 11:34:35 +1000

    Implement task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Module for flooring a float."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Function to remove decimal places and return an int type number"""
+    return math.floor(n)
