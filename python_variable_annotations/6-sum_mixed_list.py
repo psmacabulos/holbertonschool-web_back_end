380cf8d7bb61a0126cd6951c0069371590edf428
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 14:13:10 +1000

    Implement task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Module for summing mixed list."""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Method to compute the sum of a list with mixed int and floats"""
+    return sum(mxd_lst)
