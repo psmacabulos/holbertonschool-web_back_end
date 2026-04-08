6cda1e2f29bd52d46f887cd1b7248d9f071986fb
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 14:50:08 +1000

    Implement task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Module creating multiplier function."""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """REturns a function that multiplies multiplier to a float"""
+    def multiply(x: float) -> float:
+        """multiplies x to multiplier"""
+        return x * multiplier
+    return multiply
