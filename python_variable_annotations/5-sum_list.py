1fc3d17515920e663cc090a28b3ea6d0d61de6b9
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 14:01:49 +1000

    Implement task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """Module for summing a list of floats."""
+
+
+from typing import List, Tuple, Dict
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Method to get the sum of float inside a list"""
+    return sum(input_list)
