37d8439fe67dc088f2cf8aab31423c330156db42
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 20:45:12 +1000

    Implement task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Module to use Async Comprehensions"""
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> float:
+    """Method to consume stream from async_generator and return it"""
+    return [i async for i in async_generator()]
