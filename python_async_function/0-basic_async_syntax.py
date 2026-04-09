fc00ed8f7943ebe676115106c660600efbfa8391
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 16:53:16 +1000

    fix: remove typos

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int ``= 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Method to have random delay between 0 - 10 s"""
     delay = random.uniform(0, max_delay)
     asyncio.sleep(delay)
