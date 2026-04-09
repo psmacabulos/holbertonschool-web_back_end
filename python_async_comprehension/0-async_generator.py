d11de7db43f86558ce194b08a6974e1f89a99617
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 20:19:33 +1000

    Implement task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Module to create an Asynch Generator"""
+
+
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """method to run coroutine and yield a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
