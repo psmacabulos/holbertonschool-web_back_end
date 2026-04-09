684b915ab048b0dcc5ad9717e7866f26a94727c7
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 20:29:24 +1000

    fix: replace async generator to Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Module to create an Asynch Generator"""
+"""Module to create an Async Generator"""
 
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """method to run coroutine and yield a random number"""
+async def async_generator() -> Generator[float, None, None]:
+    """Yield 10 random floats asynchronously with a second delay between"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
