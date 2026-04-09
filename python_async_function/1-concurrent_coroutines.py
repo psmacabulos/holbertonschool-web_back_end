4914c45c6b6862875fc3c4f2ea8aa5d6f049ba39
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 17:55:01 +1000

    Implement task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Task 1: Execute multiple coroutines concurrently"""
 
+
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def placeholder() -> None:
-    """Placeholder function for 1-concurrent_coroutines.py."""
-    pass
-
-
-if __name__ == "__main__":
-    asyncio.run(placeholder())
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Method to ran wait_random n times and return sorted delays"""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*delays)
+    return sorted(results)
