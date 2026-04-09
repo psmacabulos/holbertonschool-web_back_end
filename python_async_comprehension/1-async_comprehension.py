6263f94a8bd05d70a9ba4ed7f980149ea605bcf4
psmacabulos <patrick.macabulos@gmail.com>
2026-04-09 20:49:21 +1000

    fix: correct return annotation to list

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """Module to use Async Comprehensions"""
 
 
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """Method to consume stream from async_generator and return it"""
     return [i async for i in async_generator()]
