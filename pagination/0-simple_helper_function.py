cce890389befe911409832db5a66e1f35f7a7de4
psmacabulos <patrick.macabulos@gmail.com>
2026-04-14 15:40:34 +1000

    Implement task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-"""Module placeholder"""
+"""Module to create a simple helper function"""
 
-from typing import List, Dict, Any, Optional
+from typing import Tuple
 
 
-# TODO: implement
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Method to return the range of idex of
+    sample pagination parameters"""
+
+    start: int = (page - 1) * page_size
+    end: int = page * page_size
+    return (start, end)
