7f97414959e6415265c0d593fe835f0ee4e7a0fe
psmacabulos <patrick.macabulos@gmail.com>
2026-04-18 16:36:09 +1000

    Implement task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module to use pymongo in inserting a document"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    return mongo_collection.insert_one(kwargs).inserted_id
