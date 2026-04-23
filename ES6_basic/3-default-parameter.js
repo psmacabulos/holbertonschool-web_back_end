4fa139058966411ab58ae7edf4599369e330710a
psmacabulos <patrick.macabulos@gmail.com>
2026-04-23 20:10:44 +1000

    fix: remove comments

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
-// How to set default parameter values in ES6
 const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => {
   return initialNumber + expansion1989 + expansion2019;
 };
