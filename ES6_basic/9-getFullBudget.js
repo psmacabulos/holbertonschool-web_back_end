ca19dbf26b5790b330611492b7f7428b19fe2b7d
psmacabulos <patrick.macabulos@gmail.com>
2026-04-25 20:31:55 +1000

    Implement task 9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,5 +1,16 @@
-#!/usr/bin/node
+import getBudgetObject from './7-getBudgetObject.js';
 
-export default function placeholder() {
-  return null;
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
 }
