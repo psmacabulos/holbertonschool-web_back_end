f6fe71ff2a2db9bca1303c10d7150ad1eaf67fb0
psmacabulos <patrick.macabulos@gmail.com>
2026-04-26 20:46:19 +1000

    Implement task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,10 +1,49 @@
-#!/usr/bin/env node
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Use setters to enforce validation
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-/**
- * 2-hbtn_course.js
- * Description: TODO - Implement feature
- */
+  // NAME
+  get name() {
+    return this._name;
+  }
 
-export default function placeholder() {
-  // TODO: Implement
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // LENGTH
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  // STUDENTS
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._students = value;
+  }
 }
