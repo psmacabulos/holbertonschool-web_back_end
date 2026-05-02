18a9a4b9695b81126212124466dff86e5113880c
psmacabulos <patrick.macabulos@gmail.com>
2026-05-02 16:36:04 +1000

    Implement task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,24 @@
-#!/usr/bin/env node
 /**
- * File: 5-typed_arrays.js
- * Description:
+ * Returns a new ArrayBuffer with an Int8 value at a specific position
+ *
+ * @param {number} length - Size of the buffer in bytes
+ * @param {number} position - Index where the value will be written
+ * @param {number} value - Integer value to store
+ *
+ * @returns {ArrayBuffer} - The resulting ArrayBuffer
  */
 
-export default function 5-typed_arrays() {
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
 
+  if (position >= length) {
+    throw new Error('Position out of range');
+  }
+  const view = new DataView(buffer);
+
+  view.setInt8(position, value);
+
+  view.set;
+
+  return buffer;
 }
