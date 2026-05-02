369bcdc78f24b21e3673978cd6284266dbf04049
psmacabulos <patrick.macabulos@gmail.com>
2026-05-02 22:16:56 +1000

    Implement task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 /**
- * File: 10-update_uniq_items.js
+ * Creates an updated map for items with initial quantity of 1
+ * 
+ * @param {Map} m - Given map
+ * 
+ * @returns {Map} - For each entry of the  map where quantity is 1
+ * the returned qty will be updated to 100
  * Description:
  */
 
-export default function 10-update_uniq_items() {
-
+export default function updateUniqueItems(m) {
+    if (!m instanceof Map) {
+        throw new Error("Cannot process")
+    }
+    for (const [key, value] of m.entries()) {
+        if (value === 1) {
+            m.set(key, 100)
+        }
+    }
 }
