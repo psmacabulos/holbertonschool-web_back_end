0c2cf9af89a8b5ca26ccf8fc85d5dea33389b318
psmacabulos <patrick.macabulos@gmail.com>
2026-05-02 21:11:05 +1000

    Implement task 7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,18 @@
-#!/usr/bin/env node
+
 /**
- * File: 7-has_array_values.js
- * Description:
+ * Checks if all elements in the array exists in the set
+ * @param {Set<any>} s - Set to check against
+ * @param {Array<any>} arr - Sample Array that will be checked against the set
+ * 
+ * @returns {boolean} True if all elements exists in the set, otherwise false
  */
 
-export default function 7-has_array_values() {
-
+const hasValuesFromArray = (s, arr) => {
+    for (const item of arr) {
+        if (!s.has(item)) return false
+    }
+    return true
 }
+
+
+export default hasValuesFromArray
