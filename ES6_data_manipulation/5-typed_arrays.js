2497fb3177d4dc3110cddcf1851175d3d417c8ad
psmacabulos <patrick.macabulos@gmail.com>
2026-05-02 16:42:34 +1000

    fix: correct error message

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -12,7 +12,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
 
   if (position >= length) {
-    throw new Error('Position out of range');
+    throw new Error('Position outside range');
   }
   const view = new DataView(buffer);
 
