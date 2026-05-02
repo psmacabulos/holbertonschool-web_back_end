267253e136a839bf3250de1b86ab7a93f50aa739
psmacabulos <patrick.macabulos@gmail.com>
2026-05-02 16:01:48 +1000

    Implement task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 /**
- * File: 4-update_grade_by_city.js
- * Description:
+ * Update the grade of students in a specific city.
+ *
+ * @param {Array<Object>} students - Array of student objects
+ * @param {string} city - City to filter students by
+ * @param {Array<Object>} newGrades - Array of objects containing studentID and grades
+ *
+ * @returns {Array<Object>} - Array of students with updated grades
  */
 
-export default function 4-update_grade_by_city() {
-
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location == city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId == student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
 }
