0b2d9ccb04859041e789a04d8bf90995e7b4b8e4
psmacabulos <patrick.macabulos@gmail.com>
2026-05-03 21:49:36 +1000

    fix: create an error object instead of string in rejecting a promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,15 @@
+/**
+ * Returns a Promise that resolves or rejects based on the input.
+ *
+ * @param {boolean} success - Determines if the Promise resolves or rejects
+ * @returns {Promise<Object>} Resolves with success response or rejects with an Error
+ */
 export default function getFullResponseFromAPI(success) {
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
         if (success) {
-            res({ 'status': 200, 'body': 'Success' })
+            resolve({ status: 200, body: 'Success' });
+        } else {
+            reject(new Error('The fake API is not working currently'));
         }
-        else {
-            rej('The fake API is not working currently')
-        }
-    })
+    });
 }
