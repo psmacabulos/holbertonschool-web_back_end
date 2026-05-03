27d69cc9d488a2b4ab86acaa9aa4393280fa0bba
psmacabulos <patrick.macabulos@gmail.com>
2026-05-03 22:00:42 +1000

    Implement task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+/**
+ * Handles a Promise and returns a standardized response.
+ *
+ * @param {Promise} promise - The Promise to handle
+ * @returns {Promise<Object>} Resolves with status and data or error
+ */
+export default function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => ({ status: 200, body: 'success' }))
+        .catch(() => new Error())
+        .finally(() => console.log("Got a response from the API to the console")
+        )
+}
