5836f10b2ce1c318f424bd586fe6653f7ac8d663
psmacabulos <patrick.macabulos@gmail.com>
2026-05-11 17:26:56 +1000

    Implement task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
+});
