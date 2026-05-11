b5761a159820cb783ee8d29aaf2ba0c35d6fa136
psmacabulos <patrick.macabulos@gmail.com>
2026-05-11 18:04:46 +1000

    fix: allow looping on input and close by ctrl + D

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
+});
 
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
