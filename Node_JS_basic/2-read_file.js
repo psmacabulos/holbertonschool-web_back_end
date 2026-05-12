cd0acba1b04ac2c6c2e5c055a119afaa0040b411
psmacabulos <patrick.macabulos@gmail.com>
2026-05-12 14:22:46 +1000

    refactor: use common js for checker

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,38 @@
-import fs from 'node:fs';
+const fs = require('fs');
 
-export default function countStudents(path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
 
-    // Make an array of students
-    let students = data.split('\n');
-    let numberOfStudents = students.length - 1;
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    // Create an array of students for each subject
-    const cs = [];
-    const swe = [];
-    for (let student of students) {
-      let details = student.split(',');
-      if (details[details.length - 1] === 'CS') {
-        cs.push(details[0]);
-      } else if (details[details.length - 1] === 'SWE') {
-        swe.push(details[0]);
-      } else continue;
-    }
+    const students = lines.slice(1);
 
-    console.log(`Number of students: ${numberOfStudents} `);
-    console.log(
-      `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
-    );
-    console.log(
-      `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`,
-    );
-  } catch (error) {
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const details = student.split(',');
+
+      const firstname = details[0];
+      const field = details[3];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstname);
+    });
+
+    Object.entries(fields).forEach(([field, names]) => {
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
+      );
+    });
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
+
+module.exports = countStudents;
