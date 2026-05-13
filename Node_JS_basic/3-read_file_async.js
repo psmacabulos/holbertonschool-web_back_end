0e72a19df627b61fe13eca002212f455930df8cc
psmacabulos <patrick.macabulos@gmail.com>
2026-05-13 18:52:03 +1000

    Implement task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            return reject(new Error('Cannot load the database'));
+        }
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        const students = lines.slice(1);
+        console.log(`Number of students: ${students.length}`);
+
+        const subjects = {};
+
+        students.forEach((student) => {
+            const details = student.split(',');
+            const name = details[0];
+            const subject = details[3];
+
+            if (!subjects[subject]) {
+                subjects[subject] = [];
+            }
+            subjects[subject].push(name);
+        });
+
+        Object.entries(subjects).forEach(([subject, studentList]) => {
+            console.log(`Number of students in ${subject}: ${studentList.length}. List: ${studentList.join(', ')}`);
+        });
+        resolve();
+    });
+});
+
+module.exports = countStudents;
