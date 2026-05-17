115ed35003f2089c424d0b70d68b5cfe52816745
psmacabulos <patrick.macabulos@gmail.com>
2026-05-17 23:07:09 +1000

    fix: remove call back of app.listen

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!\n');
 });
 const PORT = 1245;
 
-app.listen(PORT, 'localhost', () => {});
+app.listen(PORT);
 
-module.exports = { app };
+module.exports = app;
