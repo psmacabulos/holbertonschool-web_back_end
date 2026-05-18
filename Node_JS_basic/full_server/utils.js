79396239c6fd9657b1ccf5e9e10f4af6a6b3ccef
psmacabulos <patrick.macabulos@gmail.com>
2026-05-18 22:29:38 +1000

    setup file structure for task 8

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+//function to read the database
+const readDatabase = (path) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      const subjects = {};
+
+      students.forEach((student) => {
+        const details = student.split(',');
+        const name = details[0];
+        const subject = details[3];
+
+        if (!subjects[subject]) {
+          subjects[subject] = [];
+        }
+        subjects[subject].push(name);
+      });
+
+      resolve(subjects);
+    });
+  });
+
+console.log(readDatabase('../database.csv'));
+
+export default readDatabase;
